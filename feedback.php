6d2e7337c97bd16e6929c568899d5bef997771e2
LuYujin9 <wuwujane@hotmail.com>
2023-05-19 19:01:51 +0200

    accesses the data from SQL and shows them in the feedback page

--- feedback.php
+++ feedback.php
@@ -1,3 +1,24 @@
 <?php include 'inc/header.php'; ?>
+<?php // get the data from the database GET
+$sql = 'SELECT * FROM feedback';
+$result = mysqli_query($conn, $sql);
+$feedback = mysqli_fetch_all($result, MYSQLI_ASSOC);
+?>
+<h2>Feedback</h2>
+<?php if (empty($feedback)) : ?>
+    <p class="lead mt3">There is no feedback</p>
+<?php endif; ?>
+
+<?php foreach ($feedback as $item) :
+?>
+    <div class="card my-3 w-75">
+        <div class="card-body text-center">
+            <?php echo $item['body']; ?>
+            <div class="tet-secondary mt-2">
+                By <?php echo $item['name']; ?>
+            </div>
+        </div>
+    </div>
+<?php endforeach; ?>
 
 <?php include 'inc/footer.php'; ?>
